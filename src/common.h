7fbce53bfacb95b09d98f8d36f51a61a9d8ee7ca
Cole Johnson <coledjohn@gmail.com>
2025-08-08 17:16:18 -0600

    Add license and common.h

---
+++ src/common.h
@@ -0,0 +1,32 @@
+#ifndef INCLUDE_SRC_COMMON_H_
+#define INCLUDE_SRC_COMMON_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#define eprintf(...) do { fprintf(stderr, __VA_ARGS__); }while(0)
+#define UNIMPLEMENTED(...) \
+do {\
+  printf("%s:%d UNIMPLEMENTED %s", __FILE__, __LINE__, __VA_ARGS__);\
+  exit(1);\
+}while(0)
+
+#define AL_RESIZE_INC 1 << 3
+
+#define alist_last(al) (al)->items[(al)->size-1]
+
+#define alist_append(al, item) do {                                          \
+  if ((al)->capacity - (al)->size == 0) {                                    \
+    (al)->capacity += AL_RESIZE_INC;                                         \
+    (al)->items = realloc((al)->items, (al)->capacity*sizeof(*(al)->items)); \
+    assert((al)->items != NULL && "[ERROR]: Array list alloc failed!");      \
+  }                                                                          \
+  (al)->items[(al)->size++] = (item);                                        \
+}while(0)
+
+#define alist_free(al) do {   \
+ (al)->size = 0;              \
+ (al)->capacity = 0           \
+ free((al)->items);           \
+}while(0)
+
+#endif  // INCLUDE_SRC_COMMON_H_
--- src/matrix.c
+++ src/matrix.c
@@ -2,14 +2,21 @@
 
 Matrix make_matrix(uint nrows, uint ncols) {
   Matrix m = {.nrows = nrows, .ncols = ncols, .data = NULL};
-  // do all of our allocation here
   m.data = (value *)calloc(nrows * ncols, sizeof(value));
   return m;
 }
 
 void delete_matrix(Matrix *m) { free(m->data); }
 
+// nice get function to turn 1D array into proper 2D matrix
+// i is the row index and j is the col index
 value get(Matrix *m, uint i, uint j) { 
-  return 0;
+  return m->data[i*m->ncols+ j];
+}
+
+Matrix mult(Matrix *lhs, Matrix *rhs) { 
+}
+
+void for_each(Matrix *m) {
 }
 
