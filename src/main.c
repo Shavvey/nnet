8bf874280be92ec0d1b6e208644862dff155bb21
Cole Johnson <coledjohn@gmail.com>
2025-08-09 19:19:09 -0600

    Fix matrix_mult algo
    * NOTE: look into Strassen algo for better preformance

--- src/main.c
+++ src/main.c
@@ -1,17 +1,22 @@
 #include "matrix.h"
 #include <stdio.h>
 
-void print(value *val) {
-  value v = *val;
-  printf("Value is: %f\n", v);
+static void set(value *elem) {
+  *elem = 2.00;
 }
 
 int main() {
-  Matrix m = make_matrix(3, 3);
-  Matrix m1 = make_matrix(3, 3);
-  matrix_mult(&m, &m1);
-  *matrix_get(&m, 0, 1) = 3;
-  matrix_for_each(&m, &print);
-  matrix_display(&m);
+  Matrix m1 = make_matrix(2,2);
+  Matrix m2 = make_matrix(2,2);
+  *matrix_get(&m1, 0, 0) = 1;
+  *matrix_get(&m1, 1, 1) = 1;
+  matrix_for_each(&m2, &set);
+  *matrix_get(&m2, 0, 0) = 3;
+  matrix_display(&m1);
+  printf("\n");
+  matrix_display(&m2);
+  Matrix m3 = matrix_mult(&m1, &m2);
+  printf("\n");
+  matrix_display(&m3);
   return EXIT_SUCCESS;
 }
--- src/matrix.c
+++ src/matrix.c
@@ -10,10 +10,8 @@ Matrix make_matrix(uint nrows, uint ncols) {
 
 void delete_matrix(Matrix *m) { free(m->data); }
 
-// nice get function to turn 1D array into proper 2D matrix
-// i is the row index and j is the col index
-value *matrix_get(Matrix *m, uint i, uint j) {
-  return &m->data[i * m->ncols + j];
+inline value *matrix_get(Matrix *m, uint rdx, uint cdx) {
+  return &m->data[rdx * m->ncols + cdx];
 }
 
 static bool check_mdims(Matrix *lhs, Matrix *rhs) {
@@ -22,6 +20,7 @@ static bool check_mdims(Matrix *lhs, Matrix *rhs) {
   return (lncols == rnrows);
 }
 
+// NOTE: mabye implement Strassen Matrix multipliation
 Matrix matrix_mult(Matrix *lhs, Matrix *rhs) {
   if (!check_mdims(lhs, rhs)) {
     eprintf("Could not multiply matrix!\n");
@@ -33,16 +32,14 @@ Matrix matrix_mult(Matrix *lhs, Matrix *rhs) {
   Matrix res = make_matrix(lnrows, rncols);
   // preform row-column dot product version of mult
   // NOTE: maybe figure out a mult method with better preformance
-  for (uint m = 0; m < mdim; m++) {
-    for (uint r = 0; r < lnrows; r++) {
-      value mval = 0;
-      for (uint c = 0; c < rncols; c++) {
+  for (uint r = 0; r < lnrows; r++) {
+    for (uint c = 0; c < rncols; c++) {
+      *matrix_get(&res, r, c) = 0;
+      for (uint m = 0; m < mdim; m++) {
         value rval = *matrix_get(lhs, r, m);
         value cval = *matrix_get(rhs, m, c);
-        // row-col dot product
-        mval += cval * rval;
+        *matrix_get(&res, r, c) += rval * cval;
       }
-      *matrix_get(&res, r, m) = mval;
     }
   }
   return res;
@@ -53,8 +50,8 @@ void matrix_for_each(Matrix *m, void (*routine)(value *val)) {
   uint nrows = m->nrows;
   for (uint r = 0; r < nrows; r++) {
     for (uint c = 0; c < ncols; c++) {
-      value v = m->data[r * ncols + c];
-      (*routine)(&v);
+      value *v = &m->data[r * ncols + c];
+      (*routine)(v);
     }
   }
 }
@@ -64,14 +61,15 @@ void matrix_display(Matrix *m) {
   StringBuilder sb = {0};
   alist_make(&sb);
   // build sb
-  char *vstr = (char*)malloc(FLOAT_SIZE * sizeof(char));
+  char *vstr = (char *)malloc(FLOAT_SIZE * sizeof(char));
   for (uint r = 0; r < m->nrows; r++) {
     alist_append(&sb, '['); // opening brackets
     for (uint c = 0; c < m->ncols; c++) {
-      value v = *matrix_get(m, r, c); 
+      value v = *matrix_get(m, r, c);
       snprintf(vstr, FLOAT_SIZE, "%.2f", v);
       alist_append_many(&sb, vstr, FLOAT_SIZE);
-      if (c < m->ncols - 1) alist_append_many(&sb, ", ", 2);
+      if (c < m->ncols - 1)
+        alist_append_many(&sb, ", ", 2);
     }
     alist_append(&sb, ']'); // closing brackets
     alist_append(&sb, '\n');
